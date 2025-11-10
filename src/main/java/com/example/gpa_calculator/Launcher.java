e8fcdf8b9fdca494e99c0fe327ddbadda7fc1101
janir <janirafsan402@gmail.com>
2025-11-10 22:05:39 +0600

    initial commit

---
+++ src/main/java/com/example/gpa_calculator/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.gpa_calculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
